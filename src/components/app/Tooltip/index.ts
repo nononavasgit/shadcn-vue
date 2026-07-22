ed68da2e71b7975bec0c052f093060e614fc0fd0
nononavasgit <janavasgarcia1@gmail.com>
2026-07-22 10:04:00 +0200

    Tooltip

---
+++ src/components/app/Tooltip/index.ts
@@ -0,0 +1,4 @@
+export { default as Tooltip } from './Tooltip.vue'
+export { tooltipVariantes } from './variantes'
+export type { TooltipVariantes } from './variantes'
+export type { TooltipProps } from './types'

6805cef24bcbb22c39922a127af54369ce8e62e9
nononavasgit <janavasgarcia1@gmail.com>
2026-07-29 09:53:15 +0200

    refactor: make normalize composable internal

--- src/components/app/Icon/index.ts
+++ src/components/app/Icon/index.ts
@@ -1,7 +1,7 @@
 import type { IconName } from './icons.ts'
 import { cva, type VariantProps } from 'class-variance-authority'
 import type { MaybeRefOrGetter, SVGAttributes } from 'vue'
-import { useNormalize } from '@/composables'
+import { useNormalize } from '@/composables/useNormalize'
 
 export { default as Icon } from './Icon.vue'
 export type { IconName } from './icons.ts'
--- src/composables/index.ts
+++ src/composables/index.ts
@@ -1,4 +1,3 @@
 export { getContrastColor, useColor } from './useColor'
 export * from './useDates'
 export * from './useAnnouncer'
-export * from './useNormalize'

a17c7ceae9136a026ca118898361344d1128c793
nononavasgit <janavasgarcia1@gmail.com>
2026-07-31 19:42:12 +0200

    feat: normalize announcer component

--- src/components/ui/Announcer/Announcer.vue
+++ src/components/ui/Announcer/Announcer.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
-import { computed, watch } from 'vue'
+import { computed, useAttrs, watch } from 'vue'
 import { Announcer as AnnouncerBase } from '@/components/primitives/Announcer'
+import { normalizeHTMLAttributes } from '@/composables/useNormalize'
 import { useAnnouncer } from '@/composables/useAnnouncer'
+import { cn } from '@/lib/utils'
 import type { AnnouncerProps, AnnouncerSlots } from '.'
+
+defineOptions({ inheritAttrs: false })
 
 const props = withDefaults(defineProps<AnnouncerProps>(), {
   atomic: true,
   politeness: 'polite',
+  ui: undefined,
 })
 defineSlots<AnnouncerSlots>()
 
+const attrs = useAttrs()
 const { message, politeness, set, polite, assertive } = useAnnouncer({
   politeness: props.politeness,
 })
@@ -27,12 +33,27 @@ const role = computed(() => {
   if (politeness.value === 'polite') return 'status'
   return undefined
 })
+const calculatedUI = computed(() => {
+  const rootUI = normalizeHTMLAttributes(props.ui?.root)
+
+  return {
+    root: {
+      ...attrs,
+      ...rootUI,
+      'aria-atomic': props.atomic,
+      'aria-live': ariaLive.value,
+      role: role.value,
+      class: cn(attrs.class, rootUI.class),
+      style: [attrs.style, rootUI.style],
+    },
+  }
+})
 
 defineExpose({ message, politeness, set, polite, assertive })
 </script>
 
 <template>
-  <AnnouncerBase :aria-atomic="props.atomic" :aria-live="ariaLive" :role="role">
+  <AnnouncerBase v-bind="calculatedUI.root">
     <slot :message="message">{{ message }}</slot>
   </AnnouncerBase>
 </template>
--- src/components/ui/Announcer/index.ts
+++ src/components/ui/Announcer/index.ts
@@ -1,10 +1,16 @@
+import type { HTMLAttributes } from 'vue'
 import type { AnnouncerPoliteness } from '@/composables/useAnnouncer'
 
 export { default as Announcer } from './Announcer.vue'
 
+export interface AnnouncerUI {
+  root?: HTMLAttributes
+}
+
 export interface AnnouncerProps {
   atomic?: boolean
   politeness?: AnnouncerPoliteness
+  ui?: AnnouncerUI
 }
 
 export interface AnnouncerSlotProps {

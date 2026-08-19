af134073544485bc0e16cf0887596e690a055088
nononavasgit <janavasgarcia1@gmail.com>
2026-08-19 11:51:50 +0200

    Improve icon prop and size test coverage

--- tests/utils/testIconProps.ts
+++ tests/utils/testIconProps.ts
@@ -12,32 +12,44 @@ interface TestIconPropsOptions {
 interface TestIconSizeOptions {
   text: string
   id: string
+  input?: IconSize
   mount: (input: IconSize | undefined) => VueWrapper | Promise<VueWrapper>
 }
 
 export function testIconProps({ text, id, default: defaultIcon, mount }: TestIconPropsOptions) {
   it.each([
-    { input: 'check' as IconName, expected: 'check' as IconName },
-    { input: { name: 'save' as IconName }, expected: 'save' as IconName },
-    { input: undefined, expected: defaultIcon },
-  ])(`${text} input=$input`, async ({ input, expected }) => {
+    { input: 'check' as IconName },
+    { input: { name: 'save' as IconName } },
+    { input: undefined },
+  ])(`${text} input=$input`, async ({ input }) => {
     const icon = (await mount(input)).findComponent(id)
+    const expectedProps = getExpectedIconProps(input, defaultIcon)
 
-    expect(icon.exists()).toBe(expected !== undefined)
-    if (expected !== undefined) expect(icon.props('name')).toBe(expected)
+    expect(icon.exists()).toBe(expectedProps !== undefined)
+    if (expectedProps !== undefined) expect(icon.props()).toMatchObject(expectedProps)
   })
 }
 
-export function testIconSize({ text, id, mount }: TestIconSizeOptions) {
-  it.each([
-    { input: 'xs' as IconSize, expected: 'xs' as IconSize },
-    { input: 'sm' as IconSize, expected: 'sm' as IconSize },
-    { input: 'md' as IconSize, expected: 'md' as IconSize },
-    { input: 'lg' as IconSize, expected: 'lg' as IconSize },
-    { input: undefined, expected: 'md' as IconSize },
-  ])(`${text} input=$input as size=$expected`, async ({ input, expected }) => {
+function getExpectedIconProps(
+  input: NormalizeIconProps | undefined,
+  defaultIcon: IconName | undefined,
+) {
+  if (typeof input === 'string') return { name: input }
+  if (input) {
+    return {
+      name: input.name,
+      ...(input.size !== undefined && { size: input.size }),
+      ...(input.color !== undefined && { color: input.color }),
+    }
+  }
+  if (defaultIcon !== undefined) return { name: defaultIcon }
+  return undefined
+}
+
+export function testIconSize({ text, id, input = 'md', mount }: TestIconSizeOptions) {
+  it(text, async () => {
     const icon = (await mount(input)).getComponent(id)
 
-    expect(icon.props('size')).toBe(expected)
+    expect(icon.props()).toMatchObject({ size: input })
   })
 }

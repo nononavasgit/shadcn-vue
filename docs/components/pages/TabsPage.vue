bd21212a37d44aa9b7de6fb352434ba43b6f77c4
nononavasgit <janavasgarcia1@gmail.com>
2026-08-23 13:12:08 +0200

    Refactor Tabs component API and add documentation page

---
+++ docs/components/pages/TabsPage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import TabsDocs from '../docs/TabsDocs.vue'
+import TabsExamples from '../examples/tabs/TabsExamples.vue'
+import DocsLayout from '../DocsLayout.vue'
+</script>
+
+<template>
+  <DocsLayout>
+    <main class="min-h-screen bg-background p-4 text-foreground sm:p-8">
+      <div class="mx-auto grid max-w-5xl gap-6">
+        <TabsDocs />
+        <TabsExamples />
+      </div>
+    </main>
+  </DocsLayout>
+</template>
